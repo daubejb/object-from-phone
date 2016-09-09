8c2717ad65501c793b6b387e8cf8156d9df7406e
daubejb <daubejb@gmail.com>
2016-09-09 15:39:16 -0400

    create application, add custom toolbar

---
+++ app/src/main/java/com/example/root/objectfromphone/MagicActivity.java
@@ -0,0 +1,17 @@
+package com.example.root.objectfromphone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MagicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magic);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
