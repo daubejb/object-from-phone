2dc57f2953e68794de42694db7e80130ebf5883c
daubejb <daubejb@gmail.com>
2016-09-10 20:14:30 -0400

    add shake event to make quarter reappear with a noise

--- app/src/main/java/com/example/root/objectfromphone/MagicActivity.java
+++ app/src/main/java/com/example/root/objectfromphone/MagicActivity.java
@@ -1,5 +1,10 @@
 package com.example.root.objectfromphone;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,7 +15,7 @@ import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-public class MagicActivity extends AppCompatActivity {
+public class MagicActivity extends AppCompatActivity implements SensorEventListener {
 
     private final String TAG = "quarter";
     private ImageView quarterImageView;
@@ -21,21 +26,18 @@ public class MagicActivity extends AppCompatActivity {
     private int screenHeight;
     private int objWidth;
     private int objHeight;
-
+    private boolean mInitialized;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magic);
-
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-
         mainLayout = (RelativeLayout) findViewById(R.id.relative_layout);
-
         quarterImageView = (ImageView) findViewById(R.id.quarter_image_view);
-
-
         mainLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
@@ -46,19 +48,100 @@ public class MagicActivity extends AppCompatActivity {
                 objHeight = quarterImageView.getMeasuredHeight();
                 Log.d(TAG, Integer.toString(screenWidth)+"-"+Integer.toString(screenHeight));
                 Log.d(TAG, Integer.toString(objWidth)+"-"+Integer.toString(objHeight));
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(objWidth, objHeight);
-                params.leftMargin = (screenWidth - objWidth) / 2;
-                params.topMargin = (screenHeight - objHeight) / 2;
-                quarterImageView.setLayoutParams(params);
+                RelativeLayout.LayoutParams centerParams = new RelativeLayout.LayoutParams(objWidth, objHeight);
+                centerParams.leftMargin = (screenWidth - objWidth) / 2;
+                centerParams.topMargin = (screenHeight - objHeight) / 2;
+                quarterImageView.setLayoutParams(centerParams);
             }
         });
-
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         quarterImageView.setOnTouchListener(onTouchListener());
+    }
 
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
 
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(this, mSensor);
+        super.onPause();
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
+    public void onSensorChanged(SensorEvent event) {
+
+        if (quarterImageView.getVisibility() == View.INVISIBLE) {
+
+            final float XNOISE = (float) 16.0;
+            final float YNOISE = (float) 16.0;
+            final float ZNOISE = (float) 16.0;
+            double x = event.values[0];
+            double y = event.values[1];
+            double z = event.values[2];
+            double mLastX = 0.0;
+            double mLastY = 0.0;
+            double mLastZ = 0.0;
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.metal);
+
+            final double ALPHA = 0.8;
+
+            double[] gravity = {0, 0, 0};
+
+            // Isolate the force of gravity with the low-pass filter.
+            gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * event.values[0];
+            gravity[1] = ALPHA * gravity[1] + (1 - ALPHA) * event.values[1];
+            gravity[2] = ALPHA * gravity[2] + (1 - ALPHA) * event.values[2];
+
+            // Remove the gravity contribution with the high-pass filter.
+            x = event.values[0] - gravity[0];
+            y = event.values[1] - gravity[1];
+            z = event.values[2] - gravity[2];
+
+            if (!mInitialized) {
+                // sensor is used for the first time, initialize the last read values
+                mLastX = x;
+                mLastY = y;
+                mLastZ = z;
+                mInitialized = true;
+            } else {
+                // sensor is already initialized, and we have previously read values.
+                // take difference of past and current values and decide which
+                // axis acceleration was detected by comparing values
+
+                double deltaX = Math.abs(mLastX - x);
+                double deltaY = Math.abs(mLastY - y);
+                double deltaZ = Math.abs(mLastZ - z);
+                if (deltaX < XNOISE)
+                    deltaX = (float) 0.0;
+                if (deltaY < YNOISE)
+                    deltaY = (float) 0.0;
+                if (deltaZ < ZNOISE)
+                    deltaZ = (float) 0.0;
+
+                mLastX = x;
+                mLastY = y;
+                mLastZ = z;
+
+
+                if ((deltaX > deltaY) || (deltaY > deltaX) || ((deltaZ > deltaX) && (deltaZ > deltaY))) {
+                    mp.start();
+                    quarterImageView.setVisibility(View.VISIBLE);
+                    RelativeLayout.LayoutParams centerParams = new RelativeLayout.LayoutParams(objWidth, objHeight);
+                    centerParams.leftMargin = (screenWidth - objWidth) / 2;
+                    centerParams.topMargin = (screenHeight - objHeight) / 2;
+                    quarterImageView.setLayoutParams(centerParams);
+                }
+
+            }
+        }
+    }
     private View.OnTouchListener onTouchListener() {
         return new View.OnTouchListener() {
             @Override
@@ -67,10 +150,10 @@ public class MagicActivity extends AppCompatActivity {
                 final int y = (int) event.getRawY();
                 switch (event.getAction() & MotionEvent.ACTION_MASK) {
                     case MotionEvent.ACTION_DOWN:
-                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                        RelativeLayout.LayoutParams dragParams = (RelativeLayout.LayoutParams)
                                 view.getLayoutParams();
-                        xDelta = x - lParams.leftMargin;
-                        yDelta = y - lParams.topMargin;
+                        xDelta = x - dragParams.leftMargin;
+                        yDelta = y - dragParams.topMargin;
                     case MotionEvent.ACTION_UP:
                         break;
                     case MotionEvent.ACTION_MOVE:
@@ -78,8 +161,8 @@ public class MagicActivity extends AppCompatActivity {
                                 .getLayoutParams();
                         layoutParams.leftMargin = x - xDelta;
                         layoutParams.topMargin = y - yDelta;
-                        layoutParams.rightMargin = screenWidth - (x- xDelta) - quarterImageView.getWidth();
-                        layoutParams.bottomMargin = screenHeight - (y - yDelta) - quarterImageView.getHeight();
+                        layoutParams.rightMargin = screenWidth - (x- xDelta) - objWidth;
+                        layoutParams.bottomMargin = screenHeight - (y - yDelta) - objHeight;
                         view.setLayoutParams(layoutParams);
                         if ((x < 50 || x > screenWidth - 50) || (y < 50 || y > screenHeight - 50)) {
                             quarterImageView.setVisibility(View.INVISIBLE);
