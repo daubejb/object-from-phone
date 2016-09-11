f100d4663f864bebff73ea937b5d93fd271c5467
daubejb <daubejb@gmail.com>
2016-09-10 23:25:40 -0400

    create menu for application

--- app/src/main/java/com/example/root/objectfromphone/MagicActivity.java
+++ app/src/main/java/com/example/root/objectfromphone/MagicActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -59,6 +61,32 @@ public class MagicActivity extends AppCompatActivity implements SensorEventListe
         quarterImageView.setOnTouchListener(onTouchListener());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_instructions:
+                // User chose the "Instructions" item, show the instructions UI...
+                return true;
+            case R.id.action_demonstration:
+                // User chose the "Demonstration" item, take the user to youtube...
+                return true;
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
     @Override
     protected void onResume() {
         mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
